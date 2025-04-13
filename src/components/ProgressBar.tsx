b9830e1dd7a7d554e33e63752fd7114e5f0534b8
nab138 <nab@nabdev.me>
2025-04-12 22:40:21 -0400

    Add seeking

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,11 +1,12 @@
 import "./ProgressBar.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useMusic } from "../util/MusicContext";
 
 function ProgressBar() {
   const [progress, setProgress] = useState<number>(0);
   const { nowPlaying } = useMusic();
+  const progressBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     let interval: any = null;
@@ -28,8 +29,23 @@ function ProgressBar() {
     };
   }, [nowPlaying]);
 
+  const handleSeek = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!progressBarRef.current || !nowPlaying) return;
+
+    const music = MusicKit.getInstance();
+    const rect = progressBarRef.current.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+    const newProgress = clickX / rect.width;
+    music.seekToTime(newProgress * (music as any).currentPlaybackDuration);
+    setProgress(newProgress);
+  };
+
   return (
-    <div className="progress-bar-container">
+    <div
+      className="progress-bar-container"
+      ref={progressBarRef}
+      onMouseDown={handleSeek}
+    >
       <div className="progress-bar">
         <div className="progress" style={{ width: `${progress * 100}%` }}></div>
       </div>
